066cc646258139701719a0300d11da565f93c940
CBT <sushmagm25@gmail.com>
2018-02-03 16:37:51 +0530

    my first commit

---
+++ src/DemoA.java
@@ -0,0 +1,25 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class DemoA {
+
+	@Test
+	public static void main(String[] args) throws Exception {
+		
+//		URL r= new URL("http://localhost://4444/wd/hub");
+//		DesiredCapabilities d= new DesiredCapabilities();
+//		d.setBrowserName("chrome");
+//		WebDriver driver = new RemoteWebDriver(r,d);
+//		driver.quit();
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.quit();
+	}
+
+}
